7766e87d275595f267e1c75374e5a4d704f588e7
mattowsh <5089@holbertonschool.com>
2022-08-31 10:11:41 -0700

    .h update from task 0; task 0 version 0 finished

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -29,5 +31,6 @@ typedef struct binary_tree_s heap_t;
 
 /* functions */
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
