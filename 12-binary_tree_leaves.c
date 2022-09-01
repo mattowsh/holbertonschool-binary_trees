fb38b4ed25c2a5970cd771ed6b2598fd8a68cf1c
mattowsh <5089@holbertonschool.com>
2022-09-01 11:56:48 -0700

    task 12 version 0

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,5 +9,34 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t qty_leaves = 0;
 
+	if (!tree)
+		return (0);
+	return (rec_bin_tree_leaves(qty_leaves, tree));
+}
+
+/**
+ * rec_bin_tree_leaves - Calculates the quantity of leaves of a binary tree
+ * @qty_leaves : quantity of leaves
+ * @tree : a pointer to the node of the binary tree
+ *
+ * Return: the quantity of leaves
+ */
+
+size_t rec_bin_tree_leaves(size_t qty_leaves, const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0, result = 0;
+
+	if (tree->left)
+		left = rec_bin_tree_leaves(qty_leaves, tree->left);
+	if (tree->right)
+		right = rec_bin_tree_leaves(qty_leaves, tree->right);
+
+	result = left + right;
+
+	if (!tree->left && !tree->right)
+		result++;
+
+	return (result);
 }
