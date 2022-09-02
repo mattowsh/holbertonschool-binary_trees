aefa7e0c4ce7f0311428d5856db4343f6b4c6d3c
Facundoblanco10 <facub1011@gmail.com>
2022-09-02 06:36:28 -0700

    task14: fix process

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,27 +13,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	return (rec_bin_tree_balance(qty_level, tree));
+	return (balance(qty_level, tree->left) - balance(qty_level, tree->right));
 }
 
 /**
- * rec_bin_tree_balance - Calculates the balance factor of a binary tree
+ * balance - Calculates the balance factor of a binary tree
  * @qty_level : quantity of nodes
  * @tree : a pointer to the node of the binary tree
  *
  * Return: the balance factor, an int
  */
 
-int rec_bin_tree_balance(int qty_level, const binary_tree_t *tree)
+int balance(int qty_level, const binary_tree_t *tree)
 {
 	int left = 0, right = 0, result = 0;
 
 	if (tree->left)
-		left = rec_bin_tree_balance(qty_level + 1, tree->left);
+		left = balance(qty_level + 1, tree->left);
 	if (tree->right)
-		right = rec_bin_tree_balance(qty_level + 1, tree->right);
+		right = balance(qty_level + 1, tree->right);
 
-	result = left - right;
+	if (left > right)
+		result = left;
+	else
+		result = right;
 
 	if (qty_level > 0)
 		result++;
--- 14-main.c
+++ 14-main.c
@@ -9,21 +9,25 @@
  */
 int main(void)
 {
-	binary_tree_t *root;
-	int balance;
+    binary_tree_t *root;
+    int balance;
 
-	root = binary_tree_node(NULL, 98);
-	root->left = binary_tree_node(root, 50);
-	root->right = binary_tree_node(root, 100);
-	root->left->left = binary_tree_node(root->left, 40);
-	root->left->right = binary_tree_node(root->left, 60);
-	/*root->right->left = binary_tree_node(root->right, 99);*/
-	/*root->right->right = binary_tree_node(root->right, 110);*/
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root, 128);
+    binary_tree_insert_left(root, 45);
+    binary_tree_insert_right(root->left, 50);
+    binary_tree_insert_left(root->left->left, 10);
+    binary_tree_insert_left(root->left->left->left, 8);
+    binary_tree_print(root);
 
-	balance = binary_tree_balance(root);
-	printf("Balance factor for tree with root node (%d): %+d\n", root->n, balance);
-
-	binary_tree_print(root);
-	binary_tree_delete(root);
-	return (0);
+    balance = binary_tree_balance(root);
+    printf("Balance of %d: %+d\n", root->n, balance);
+    balance = binary_tree_balance(root->right);
+    printf("Balance of %d: %+d\n", root->right->n, balance);
+    balance = binary_tree_balance(root->left->left->right);
+    printf("Balance of %d: %+d\n", root->left->left->right->n, balance);
+    return (0);
 }
